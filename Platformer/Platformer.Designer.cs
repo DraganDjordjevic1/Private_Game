0888de655d2ccceb5729cf817e121dd8e08d5c6c
DraganDjordjevic1 <101023448@student.swin.edu.au>
2017-11-23 21:22:52 +1100

    Add project files.

---
+++ Platformer/Platformer.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Platformer
+{
+    partial class Platformer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.Block = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Block)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.Block);
+            this.screen.Controls.Add(this.player);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(821, 436);
+            this.screen.TabIndex = 0;
+            // 
+            // Block
+            // 
+            this.Block.BackColor = System.Drawing.Color.Gray;
+            this.Block.Location = new System.Drawing.Point(130, 239);
+            this.Block.Name = "Block";
+            this.Block.Size = new System.Drawing.Size(578, 57);
+            this.Block.TabIndex = 3;
+            this.Block.TabStop = false;
+            this.Block.Click += new System.EventHandler(this.Block_Click);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Black;
+            this.player.Location = new System.Drawing.Point(27, 387);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(25, 25);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Platformer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 436);
+            this.Controls.Add(this.screen);
+            this.Name = "Platformer";
+            this.Text = "Platform";
+            this.Load += new System.EventHandler(this.Platformer_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Platformer_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Platformer_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Block)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Block;
+    }
+}
+
---
+++ Platformer/Platformer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platformer
+{
+    public partial class Platformer : Form
+    {
+        bool right, left;
+        bool jump;
+        int G = 30;
+        int Force;
+         
+        public Platformer()
+        {
+            InitializeComponent();
+
+            player.Top = screen.Height - player.Height; //sets player start position
+        }
+
+        private void Platformer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 1. side collision
+            if (player.Right > Block.Left && player.Left < Block.Right - player.Width / 2 && player.Bottom > Block.Top )
+            {
+                right = false;
+            }
+
+            if (player.Left < Block.Right && player.Right > Block.Left + player.Width / 2 && player.Bottom > Block.Top)
+            {
+                left = false;
+            }
+
+            //////
+            // 2. 
+            if (right == true) 
+            {
+                player.Left += 3;
+            }
+
+            if (left == true)
+            {
+                player.Left -= 3;
+            }
+
+            if (jump == true)
+            {
+                //falling (if the player has jumped before)
+                player.Top -= Force;
+                Force -= 1; 
+            }
+
+            if (player.Top + player.Height >= screen.Height)
+            {
+                player.Top = screen.Height - player.Height; //stop falling at bottom 
+                jump = false;
+            }
+            else
+            {
+                player.Top += 5; //Falling
+            }
+
+            //3. top collision
+            if (player.Left + player.Width > Block.Left && player.Left + player.Width < Block.Left + Block.Width+ player.Width && player.Top + player.Height >= Block.Top && player.Top < Block.Top)
+            {
+                jump = false;
+                Force = 0;
+                player.Top = Block.Location.Y - player.Height;
+            }
+            //head collision
+             
+            if (player.Left + player.Width > Block.Left && player.Left + player.Width < Block.Left + Block.Width + player.Width && player.Top - Block.Bottom <= 10 && player.Top - Block.Top > -10)
+            {
+                Force = -1;
+            }
+            //////
+        }
+
+        private void Platformer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = true; 
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true; 
+            }
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();  //Escape -> Exit
+            }
+
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    Force = G;
+                }
+            }
+        }
+
+        private void Block_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Platformer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+            }
+        }
+    }
+}
